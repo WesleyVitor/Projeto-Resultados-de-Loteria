3c676de89eb41955ab649494e8d2fe9a96ca702d
WesleyVitor <wesleydemorais@outlook.com.br>
2021-12-27 13:55:04 -0300

    make responsive sidebar and sideball components

--- src/Components/SideBar.js
+++ src/Components/SideBar.js
@@ -2,12 +2,13 @@ import React from 'react';
 import "./SideBar.css";
 import SelectBox from './SelectBox';
 const SideBar = ({contests, handleContestChosen, contestChosen,infoContest})=>{
+    const name_sidebar = contests.length >0 ? contests[contestChosen].nome : "Mega-sena";
     return (
         <div className='sidebar'>
             <SelectBox contests={contests} handleContestChosen={handleContestChosen}/> 
             <h1>
                 <img style={{height:'55px', width:"59px"}} src='/assets/img/Logo_Sena.png' alt='logo'/>
-                {contests.length >0 ? contests[contestChosen].nome : "Mega-sena"}
+                {name_sidebar}
             </h1>
             <div>
                 <span id="concurso">Concurso</span>
